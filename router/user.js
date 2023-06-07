e19011ea90d0712c5b2e3b3c81595aca603c4e95
FeifanW <qihang791@163.com>
2023-06-07 23:49:31 +0800

    练习express-validator

--- router/user.js
+++ router/user.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const userCtrl = require('../controller/user');
 const { query, validationResult, body } = require('express-validator');
+const { User } = require('../model/user');
 
 const router = express.Router();
 
@@ -10,9 +11,29 @@ router.post(
   '/users/register',
   [
     // 1.配置验证规则
-    body('user.username').notEmpty(),
-    body('user.password').notEmpty(),
-    body('user.email').notEmpty(),
+    body('user.username')
+      .notEmpty()
+      .withMessage('用户名不能为空')
+      .custom(async (username) => {
+        const user = await User.findOne({ username });
+        if (user) {
+          return Promise.reject('用户名已存在');
+        }
+      }),
+    ,
+    body('user.password').notEmpty().withMessage('密码不能为空'),
+    body('user.email')
+      .notEmpty()
+      .withMessage('邮箱不能为空')
+      .isEmail()
+      .withMessage('邮箱格式不正确')
+      .bail()
+      .custom(async (email) => {
+        const user = await User.findOne({ email });
+        if (user) {
+          return Promise.reject('邮箱已存在');
+        }
+      }),
   ],
   (req, res, next) => {
     // 2.判断验证结果
